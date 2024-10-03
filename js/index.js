bce02a4a2156d3486dd4509d059fd68895e449c7
аппле <apple@MacBook-Air-apple.local>
2024-10-03 08:58:49 +0500

    Qo'shimcha masalalalr

---
+++ js/index.js
@@ -0,0 +1,218 @@
+// 1-masala
+// let num =+prompt("Sonni kiriting") ;
+// if (num>0) {
+//     console.log("Musbat");
+// } else {
+//     console.log("Manfiy");
+// }
+
+// 2-masala
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// if (num1 % 2 == 0 && num2 % 2 == 0) {
+//     console.log("Juft sonlar");
+// } else {
+//     console.log("Juft sonlar emas"); 
+// }
+
+// // 3-masala
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+// let num3 = +prompt("3-sonni kiriting");
+
+// if (num1 == num2 && num1 == num3 && num2 == num3 ) {
+//     console.log("Barcha sonlar teng");
+// } else {
+//     console.log("Barcha sonlar teng emas");
+// }
+
+// 4-masala
+
+// let num = +prompt("Sonni kiriting") ;
+// if (num>0 && num<100) {
+//     console.log("O'rtacha son");
+// } else {
+//     console.log("Chetdan tashqarida"); 
+// } 
+
+// 5-masala
+
+// let num = +prompt("Haroratni kiriting") ;
+
+// if (num<0) {
+//     console.log("Muzlash");
+// } else {
+//     console.log("Oddiy");
+// }
+
+// 6-masala
+
+// let num = +prompt("Sonni kriiting") ;
+
+// if (num % 2 == 1) {
+//     console.log("Toq");
+// } else {
+//     console.log("Juft");
+// }
+
+// 7-masala
+
+// let num = +prompt("Sonni kriiting") ;
+
+// if (num % 5 == 0) {
+//     console.log("Bo'linadi");
+// } else {
+//     console.log("Bo'linmaydi");
+// }
+
+// 8-masala
+
+// let age = +prompt("Yoshingizni kiriting") ;
+// if (age => 18) {
+//     console.log("Voyaga yetgan");
+// } else {
+//     console.log("Voyaga yetmagan");
+// }
+
+// 9-masala
+
+// let year = +prompt("Yilni kiriting") ;
+// if (year % 4 == 0) {
+//     console.log("Kabisa yili");
+// } else {
+//     console.log("Kabisa yili emas");
+// }
+
+// 10-masala
+
+// let num1 = +prompt("1-sonni kiriting");
+// let num2 = +prompt("2-sonni kiriting");
+
+// if (num1 > 0 && num2 < 0 || num2>0 && num1<0) {
+//     console.log("Bittasi musbat bittasi manfiy");
+// } else {
+//     console.log("HAr xil emas");
+// }
+
+// 11-masala
+// let height = +prompt("Balandlikni kiriting");
+// let weight = +prompt("Ogirlikni kirting");
+
+// if (height => 175 && weight >= 75) {
+//     console.log("Baland va og'ir");
+// } else {
+//     console.log("Oddiy");
+// }
+
+// 12-masala
+
+// let week = +prompt("1 dan 7 gacha sonlarni kiriting");
+// if (week == 6 || week == 7) {
+//     console.log("Dam olish kuni");
+// } else if (week == 1 || week == 2 || week == 3 || week == 4 || week == 5) {
+//     console.log("Ish kuni");
+// } else {
+//     console.log("Notogri son kiritding");
+// }
+
+// 13-masala
+
+// let clock = +prompt("Soatni kiriting") ;
+
+// if (clock == 12) {
+// console.log("Tush payti");
+// } else {
+//     console.log("Tush emas");
+// }
+
+// 14-masala
+// let grade = +prompt("Bahoni kiriting") ;
+
+// if (grade>=5) {
+//     console.log("O'tgan");
+// } else {
+//     console.log("O'tmagan");
+// }
+
+// 15-masala
+
+// let num1 = +prompt("1-sonni kiriting") ;
+// let num2 = +prompt("2-sonni kiriting") ;
+// if (num1+num2 >= 100 ){
+// console.log("Yig'indi katta");
+// } else {
+//     console.log("Yig'indi kichik");
+// }
+
+// 16-masala
+
+// let num = +prompt("Sonni kiriting");
+
+// if (num % 2 == 0 && num > 0) {
+//     console.log("Musbat va juft");
+// } else {
+//     console.log("Musbat va Juft emas"); 
+// }
+
+// 17-masala
+
+// let age = +prompt("Sonni kiriting") ;
+// if (age => 18 && age <= 65) {
+//     console.log("Ishga yaroqli");
+// } else {
+//     console.log("IShga yaroqsiz");  
+// }
+
+// 18-masala
+
+// let season = prompt("Yil faslini kiriting (bahor , yoz , kuz ,qish) :")
+
+// if (season === "yoz") {
+//     console.log("Issiq");
+// } else {
+//     console.log("Issiq emas"); 
+// }
+
+// 19-masala
+
+// let number = +prompt("1 dan 12 gacha sonlarni kiriting") ;
+
+// if (number === 6 || number === 7 || number === 8 || number === 9 || number === 10 || number === 11 || number === 12 ) {
+//     console.log("Ertalab");
+// } else {
+//     console.log("Kun emas");
+// }
+
+// For sikliga oid masalalar
+// 1-masala
+// for (let i = 1 ; i<=10 ; i++) {
+//     console.log(i);
+// }
+
+// 2-masala
+// for (let i = 10 ; i >= 1 ; i--) {
+// console.log(i);
+// }
+
+// 3-masala
+for (let i =2 ; i<=100 ; i+2) {
+    console.log(i);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
